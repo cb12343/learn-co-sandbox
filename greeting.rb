be9482b0e7f9a7a312de893369174f6e40bd432f
clbowling12@gmail.com <clbowling12@gmail.com>
2018-06-07 16:56:50 +0000

    automatically backed up by learn

---
+++ greeting.rb
@@ -0,0 +1,21 @@
+def greeting
+  puts "Hello World"
+
+
+end
+greeting
+greeting
+greeting
+greeting
+
+answer = "< flamboyance >"
+puts "A group of flamingos is called a #{answer}."
+end
+
+def stylish_chef
+  best_hairstyle = "Guy Fieri"
+  return "Martha Stewart"
+  "Guy Fieri"
+end
+
+
